81926bd637feab3c69acbb2cc3af1a0238f67489
Eugene <6ecuk@users.noreply.github.com>
2014-08-28 15:30:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 var  emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/;
+var  counter = 0;
 function Forms(nameFormData, emailFormData) {
  this.nameForm = nameFormData;
  this.emailForm = emailFormData;
 }
-function Ticket(emailData, nameData, messageData) {
 
+function Sticker(emailData, nameData, messageData, counter) {
     this.email = emailData;
     this.name = nameData;
     this.message = messageData;
     this.div = document.createElement('div');
     this.p = document.createElement('p');
+    this.deleteButton = document.createElement('a');
     this.parrentDiv = document.getElementById('messageBox');
     this.div.className = 'messageView';
+    this.div.id = "sticker_" + counter;
     this.p.className = 'message_p';
+    this.deleteButton.className = 'delete';
+    this.deleteButton.href = '#';
+    this.deleteButton.id = this.parrentDiv.childElementCount;
+    this.deleteButton.onclick = removeSticker;
     this.p.innerHTML = this.message;
+    this.div.appendChild(this.deleteButton);
     this.div.appendChild(this.p);
     this.parrentDiv.insertBefore(this.div, this.parrentDiv.firstChild);
+
 }
+
+function removeSticker () {
+    var parentDiv = document.getElementById('messageBox');
+    var deleteDiv = document.getElementById(this.parentElement.id);
+    parentDiv.removeChild(deleteDiv);
+//   console.log(parentDiv);
+}
+
 function createObjects() {
     forms = new Forms(
         document.getElementById("nameText"),
@@ -47,16 +64,22 @@ function createObjects() {
         forms.nameForm.value = "Имя должно содержать более 3 символов";
     }
 else {
-    ticket = new Ticket(
+
+    sticker = new Sticker(
         document.getElementById("emailText").value,
         document.getElementById("nameText").value,
-        document.getElementById("messageText").value
+        document.getElementById("messageText").value,
+        counter
     );
+    counter++;
 
     }
 }
+
 
 window.onload = function () {
 document.getElementById('button').onclick = createObjects;
 
 };
+
+
